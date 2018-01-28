5984ffd9018caa2a476307f3045d5909ed8d6126
ceej01 <ceej.carmean@tuta.io>
2018-01-28 15:12:25 -0700

    Add Order of operation, cleaner code

--- calculator.js
+++ calculator.js
@@ -1,38 +1,69 @@
 let buttons = document.querySelectorAll('.number, .operator');
-let input = '';
+
 let displayValue = '';
-let storeVals = [];
-let storeOpers = [];
-let isOperating = false;
+let valArray = [];
+let operArray = [];
+let isOperating = true;
 
 
 //Display value of button pushed
-buttons.forEach((button) => { 
+buttons.forEach((button) => { 8
   button.addEventListener('click', () => { 
-	input = button.value;
+	let input = button.value;
 
+	/*
+		1) First Input
+		2) Operation
+		3) Second Input
+		4) Evaluate or Operation
+			a) If (Eval):
+				-> Evaluate
+				-> Display number
+				-> Ready for next Operation
+			b) If (Oper):
+				-> Evaluate
+				-> Display number
+				-> Ready for next Operation
+
+		** If input== '=', Don't push
+
+
+	*/
 	if (input >= '0' && input <= '9'){
 		displayValue = Number("" + displayValue + input); //appends each digit to the end
-		console.log(displayValue);
+		//Update <p id=displayValue> here:
+		
+		console.log("dV: " + displayValue);
 		isOperating = false;
 	}
 
 	else {
 		if (isOperating == false) { //So an operator can only be hit once. ie only between numbers
-			storeVals.push(displayValue);			
-			storeOpers.push(input);
-			console.log("Operations: " + storeOpers + ", storeVals: " + storeVals);
-			operate(input, storeVals[(storeVals.length) - 2], storeVals[(storeVals.length) - 1]);
-			displayValue = "";
+			if(isInt(displayValue)){
+				valArray.push(displayValue);
+				displayValue = "";
+				console.log(valArray);
+			}
+			switch (input){
+				case '=' :
+					displayValue = orderOperations(valArray,operArray);
+					valArray.push(displayValue);
+					break;
+
+				default :
+					operArray.push(input);
+					console.log(operArray);
+					break;
+			}
 			isOperating = true;
-		}	
-	}
-  });
-});
+		}
+	}	
+   })
+  })
 
 function add (a, b) {
-	parseInt(a,10);
-	parseInt(b,10);
+	//parseInt(a,10);
+	//parseInt(b,10);
 	return a + b;
 }
 
@@ -69,22 +100,44 @@ function factorial(n) {
 	return total;
 }
 
-function operate(operand, a, b) {
-	switch(operand) {
-		case "+":
-			console.log("add: " + add(a,b));
-			break;
-		case "-":
-			console.log(subtract(a,b));
-			break;
-		case "*":
-			console.log(multiply(a,b));
-			break;
-		case "/":
-			console.log(divide(a,b));
-			break;	
-	}
+function orderOperations(numArray, operArray) {
+	/* 	1) get indexes of each: *,/,+,-
+		2) store array of every index of each symbol
+		3) geom[i,j,k,...]
+		4) operate(operArray[i,k,k...],numArray[i,j,k...],numArray[i+1,j+1,k+1])
+		5) consense array after every operation
+		4) arth[n,m,o...]
+	*/
 
+	let multIndices = [];
+	let arthIndices = [];
+
+	for (var i = 0; i < operArray.length; i++) {
+	 	if (operArray[i] == '*' ||  operArray[i] == '/'){
+	 		multIndices.push(i);
+	 	}
+	 	if (operArray[i] == '+' ||  operArray[i] == '-'){
+	 		arthIndices.push(i);
+	 	}
+	}
+	console.log("* & /: " + multIndices);
+	console.log("+ & -: " + arthIndices);
+}
+
+function operate(operand, a , b) {
+
+
+}
+
+
+
+
+
+function isInt(value) {
+	// https://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript#14794066
+  	return !isNaN(value) && 
+         	parseInt(Number(value)) == value && 
+         	!isNaN(parseInt(value, 10));
 }
 
 /*
